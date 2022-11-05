ae77cc01a317e91f872ab313ba8ed7590f6a178f
Andrzej <AndrzejS21@interia.pl>
2022-11-05 17:22:13 +0100

    Add project files.

---
+++ FormUI/Dashboard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormUI
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database1DataSet);
+
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.database1DataSet.Customers);
+
+        }
+
+        private void genderCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (genderCheckBox.CheckState == CheckState.Checked)
+                genderCheckBox.Text = "Male";
+            else if (genderCheckBox.CheckState == CheckState.Checked)
+                genderCheckBox.Text = "Famale";
+            else genderCheckBox.Text = "??";
+        }
+    }
+}
